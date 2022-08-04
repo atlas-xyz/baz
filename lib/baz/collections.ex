2dcf499a2db68e175b44ac7e22e54578a7768499
Alex Kwiatkowski <alex+git@fremantle.io>
2022-08-04 02:32:47 -0700

    add create_collection to facade

--- lib/baz/collections.ex
+++ lib/baz/collections.ex
@@ -5,7 +5,7 @@ defmodule Baz.Collections do
 
   @type where_opt :: {:where, [{atom, term}]}
   @type order_opt :: {:order, [atom]}
-  @type collection :: struct
+  @type collection :: Collection.t()
 
   @type filter_and_order_opts :: [where_opt | order_opt]
   @spec filter_and_order(filter_and_order_opts) :: [collection]
@@ -35,6 +35,14 @@ defmodule Baz.Collections do
   @spec collection_changeset(map) :: Ecto.Changeset.t() | {:error, term}
   def collection_changeset(attrs) do
     Collection.changeset(%Collection{}, attrs)
+  end
+
+  @type create_attrs :: map
+  @spec create_collection(create_attrs) :: {:ok, collection} | {:error, term}
+  def create_collection(attrs) do
+    %Collection{}
+    |> Collection.changeset(attrs)
+    |> Repo.insert()
   end
 
   @doc """
