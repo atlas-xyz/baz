89e914ec6d7cad575732224e3297a58f6bcd0393
Irina Guberman <irina.guberman@xaptum.com>
2022-09-02 10:58:57 -0500

    Ensure unique events

--- lib/baz/normalized_sinks/timescale.ex
+++ lib/baz/normalized_sinks/timescale.ex
@@ -60,7 +60,7 @@ defmodule Baz.NormalizedSinks.Timescale do
 
   @impl true
   def receive_collection_event_import(multi) do
-    Repo.transaction(multi, on_conflict: :nothing)
+    Repo.transaction(multi)
   end
 
   @impl true
