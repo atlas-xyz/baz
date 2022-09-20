1d128b26cc2ccdf12ecc804dd1534b4b1f55bf77
Alex Kwiatkowski <alex+git@fremantle.io>
2022-09-20 12:19:01 -0700

    release v0.0.17

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ Add the `baz` package to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:baz, "~> 0.0.16"}
+    {:baz, "~> 0.0.17"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Baz.MixProject do
   def project do
     [
       app: :baz,
-      version: "0.0.16",
+      version: "0.0.17",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
